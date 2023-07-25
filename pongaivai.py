b8dc5aec18976d28e97fe97cf3566c44188727b4
Katherine Devine <140209312+katiedevine@users.noreply.github.com>
2023-07-25 14:54:12 -0400

    Update and rename PongAIvAI.py to pongaivai.py

--- pongaivai.py
+++ pongaivai.py
@@ -1,8 +1,8 @@
-#   PongAIvAI
+#   pongaivai
 
-# This program is designed to conduct a game of pong between Chaser_AI player and Student_AI player
+# this program is designed to conduct a game of pong between chaser_ai player and student_ai player
 
-#   Remarks: Parts of the code are based on T. S. Hayden Dennison's PongClone (2011)
+#   remarks: Parts of the code are based on T. S. Hayden Dennison's PongClone (2011)
 #   http://www.pygame.org/project-PongClone-1740-3032.html
 
 import os, sys, time, math, random
@@ -18,40 +18,40 @@ black = [0, 0, 0]
 clock = pygame.time.Clock()
 
 class Rectf:
-    # A class that stores coordinates of a rectangle in floating-point
+    # a class that stores coordinates of a rectangle in floating-point
     # with useful methods that can be performed on them
     
     def __init__(self, pos, size):
-        # Create a rectangle with the given (x, y) position
+        # create a rectangle with the given (x, y) position
         # and (height, width) size.
         
         self.pos = (pos[0], pos[1]) # (x, y)
         self.size = (size[0], size[1]) # (width, height)
     
     def move(self, x, y):
-        # Move this rectangle by +x, +y units and return the new rectangle.
-        # Size is kept the same.
+        # move this rectangle by +x, +y units and return the new rectangle.
+        # size is kept the same.
         
         return Rectf((self.pos[0]+x, self.pos[1]+y), self.size)
 
     def move_ip(self, x, y, move_factor = 1):
-        # Move this rectangle by +x*factor, +y*factor units in-place.
+        # move this rectangle by +x*factor, +y*factor units in-place.
         
         self.pos = (self.pos[0] + x*move_factor, self.pos[1] + y*move_factor)
 
     def get_rect(self):
-        # Return a copy (clone) of this rectangle cast to a PyGame Rect object.
+        # return a copy (clone) of this rectangle cast to a PyGame Rect object.
         
         return Rect(self.pos, self.size)
 
     def copy(self):
-        # Return a copy (clone) of this rectangle.
+        # return a copy (clone) of this rectangle.
         
         return Rectf(self.pos, self.size)
 
     def intersect(self, other_Rectf):
-        # Check if this rectangle intersects with other_Rectf.
-        # Two rectangles intersect iff both x and y projections intersect.
+        # check if this rectangle intersects with other_Rectf.
+        # two rectangles intersect iff both x and y projections intersect.
         
         for i in range(2):
             if self.pos[i] < other_Rectf.pos[i]: # projection of self begins to the left
@@ -64,9 +64,9 @@ class Rectf:
         
 class Paddle:
     def __init__(self, pos, size, speed, max_angle,  facing, timeout):
-        # Create a new Paddle.
+        # create a new paddle
         
-        # The paddle is a rectangular object, so store its coordinates
+        # the paddle is a rectangular object, so store its coordinates
         # in a Rectf object.
         self.Rectf = Rectf((pos[0]-size[0]/2, pos[1]-size[1]/2), size)
         
@@ -80,14 +80,12 @@ class Paddle:
         self.speed = factor*self.speed
 
     def move(self, enemy_Rectf, ball_Rectf, table_size):
-        # Move the paddle.
+        # move the paddle.
         
-        # The program crashes if move_getter crashes. The runtime of 
-        # move_getter is not limited
+        # the program crashes if move_getter crashes if the runtime of move_getter is not limited
         direction = self.move_getter.pong_ai(self.Rectf.copy(), enemy_Rectf.copy(), ball_Rectf.copy())
         
-        # The program continues if move_getter crashes. The runtime of
-        # move_getter is limited
+        # the program continues if move_getter crashes, The runtime of move_getter is limited
         # direction = timeout(self.move_getter.pong_ai, (self.Rectf.copy(), enemy_Rectf.copy(), ball_Rectf.copy()), {}, self.timeout)
         
         if direction == "up":
@@ -109,10 +107,10 @@ class Paddle:
                 )
 
     def get_angle(self, y):
-        # For a ball hitting the paddle at the given y coordinate,
+        # for a ball hitting the paddle at the given y coordinate,
         # return the angle at which the ball should bounce back.
         
-        # Determine the y-coordinate for the center of the paddle.
+        # determine the y-coordinate for the center of the paddle.
         center = self.Rectf.pos[1]+self.size[1]/2
         
         # How much does the given y deviate from the center?
@@ -127,16 +125,16 @@ class Paddle:
 
 class Ball:
     def __init__(self, table_size, size, paddle_bounce, wall_bounce, dust_error, init_speed_mag):
-        # Create a new ball object.
+        # create a new ball object
         
-        # Start with a random (x, y) speed vector.
+        # start with a random (x, y) speed vector
         rand_ang = (.4+.4*random.random())*math.pi*(1-2*(random.random()>.5))+.5*math.pi
         speed = (init_speed_mag*math.cos(rand_ang), init_speed_mag*math.sin(rand_ang))
         
-        # Start the ball in the middle of the board.
+        # start the ball in the middle of the board
         pos = (table_size[0]/2, table_size[1]/2)
         
-        # Rectangle representing the ball's position.
+        # rectangle representing the ball's position
         self.Rectf = Rectf((pos[0]-size[0]/2, pos[1]-size[1]/2), size)
         
         self.speed = speed
@@ -148,12 +146,12 @@ class Ball:
         self.prev_bounce = None
 
     def get_center(self):
-        # Return the center (x, y) coordinate of the ball.
+        # return the center (x, y) coordinate of the ball
         
         return (self.Rectf.pos[0] + .5*self.Rectf.size[0], self.Rectf.pos[1] + .5*self.Rectf.size[1])
 
     def get_speed_mag(self):
-        # Return the magnitude of the speed vector.
+        # return the magnitude of the speed vector
         
         return math.sqrt(self.speed[0]**2+self.speed[1]**2)
 
@@ -161,15 +159,15 @@ class Ball:
         self.speed = (factor*self.speed[0], factor*self.speed[1])
 
     def move(self, paddles, table_size, move_factor):
-        # Move the ball.
+        # move the ball
         
         moved = 0
         
-        # The borders of the window serve as impassable walls.
+        # the borders of the window serve as impassable walls.
         walls_Rects = [Rect((-100, -100), (table_size[0]+200, 100)),
                        Rect((-100, table_size[1]), (table_size[0]+200, 100))]
 
-        # Check if the ball has collided with any of the walls.
+        # check if the ball has collided with any of the walls.
         for wall_rect in walls_Rects:
             if self.Rectf.get_rect().colliderect(wall_rect):
                 # A collision did occur.
@@ -189,10 +187,10 @@ class Ball:
                     c -= 1 # move by roughly the same amount as the ball had traveled into the wall
                 moved = 1
                 
-        # Check if the ball has collided with a paddle.
+        # check if the ball has collided with a paddle
         for paddle in paddles:
             if self.Rectf.intersect(paddle.Rectf):
-                # A collision did occur.
+                # a collision did occur
                 
                 if (paddle.facing == 1 and self.get_center()[0] < paddle.Rectf.pos[0] + paddle.Rectf.size[0]/2) or \
                 (paddle.facing == 0 and self.get_center()[0] > paddle.Rectf.pos[0] + paddle.Rectf.size[0]/2):
@@ -218,13 +216,13 @@ class Ball:
                               math.cos(-theta)*v[1]+math.sin(-theta)*v[0]]
 
 
-                # Bona fide hack: enforce a lower bound on horizontal speed and disallow back reflection
+                # bona fide hack: enforce a lower bound on horizontal speed and disallow back reflection
                 if  v[0]*(2*paddle.facing-1) < 1: # ball is not traveling (a) away from paddle (b) at a sufficient speed
                     v[1] = (v[1]/abs(v[1]))*math.sqrt(v[0]**2 + v[1]**2 - 1) # transform y velocity so as to maintain the speed
                     v[0] = (2*paddle.facing-1) # note that minimal horiz speed will be lower than we're used to, where it was 0.95 prior to the  increase by 1.2
 
-                #a bit hacky, prevent multiple bounces from accelerating
-                #the ball too much
+                #prevent multiple bounces from accelerating the ball too much
+                
                 if not paddle is self.prev_bounce:
                     self.speed = (v[0]*self.paddle_bounce, v[1]*self.paddle_bounce)
                 else:
@@ -246,7 +244,7 @@ class HumanInput():
         self.table_size = table_size
     
     def pong_ai(self, paddle_rect, other_paddle_rect, ball_rect):
-        # Handle user input (up/down arrows).
+        # handle user input (up/down arrows)
         
         keys = pygame.key.get_pressed()
 
@@ -282,29 +280,29 @@ def timeout(func, args=(), kwargs={}, timeout_duration=1, default=None):
         return it.result
 
 def render(screen, paddles, ball, score, table_size):
-    # Draw the Pong board to the screen.
+    # draw the Pong board to the screen
     screen.fill(black) # fill screen with black color
 
-    # Draw paddle rectangles.
+    # draw paddle rectangles
     pygame.draw.rect(screen, white, paddles[0].Rectf.get_rect())
     pygame.draw.rect(screen, white, paddles[1].Rectf.get_rect())
 
-    # Draw circle for ball.
+    # draw circle for ball
     pygame.draw.circle(screen, white, (int(ball.get_center()[0]), int(ball.get_center()[1])),  int(ball.Rectf.size[0]/2), 0)
 
-    # Draw line separating sides.
+    # draw line separating sides
     pygame.draw.line(screen, white, [screen.get_width()/2, 0], [screen.get_width()/2, screen.get_height()])
 
-    # Draw scores
+    # draw scores
     score_font = pygame.font.Font(None, 32)
     screen.blit(score_font.render(str(score[0]), True, white), [int(0.4*table_size[0])-8, 0])
     screen.blit(score_font.render(str(score[1]), True, white), [int(0.6*table_size[0])-8, 0])
 
-    # Flip drawing to screen
+    # flip drawing to screen
     pygame.display.flip()
 
 def check_point(score, ball, table_size):
-    # Check if a point has been scored and return the updated score.
+    # check if a point has been scored and return the updated score
     
     if ball.Rectf.pos[0]+ball.size[0]/2 < 0:
         score[1] += 1
@@ -319,22 +317,22 @@ def check_point(score, ball, table_size):
 
 
 def game_loop(screen, paddles, ball, table_size, clock_rate, turn_wait_rate, score_to_win, display):
-    # Main game loop.
+    # main game loop
     
     score = [0, 0]
 
-    # Continue until a player has reached max score (won the game)
+    # continue until a player has reached max score (won the game)
     while max(score) < score_to_win:
         old_score = score[:]
         
-        # Check if a point has been scored.
+        # check if a point has been scored
         ball, score = check_point(score, ball, table_size)
         
-        # Move the paddles.
+        # move the paddles
         paddles[0].move(paddles[1].Rectf, ball.Rectf, table_size)
         paddles[1].move(paddles[0].Rectf, ball.Rectf, table_size)
         
-        # Move the ball.
+        # move the ball
         inv_move_factor = int((ball.speed[0]**2+ball.speed[1]**2)**.5)
         if inv_move_factor > 0:
             for i in range(inv_move_factor):
